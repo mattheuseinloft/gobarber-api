0fa65243e1d2a487c972041eb677c04edbbfb2d4
Mattheus Einloft <mattheuseinloft@gmail.com>
2020-07-05 18:32:17 -0300

    add user_id field to appointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -17,11 +17,12 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
-      provider_id: '121231231',
+      user_id: '123123',
+      provider_id: '123123',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('121231231');
+    expect(appointment.provider_id).toBe('123123');
   });
 
   it('should not be able to create two appointments on the same time', async () => {
@@ -29,13 +30,15 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
-      provider_id: '121231231',
+      user_id: '123123',
+      provider_id: '123123',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
-        provider_id: '121231231',
+        user_id: '123123',
+        provider_id: '123123',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
